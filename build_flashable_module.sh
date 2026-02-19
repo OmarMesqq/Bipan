92dc72484d134f954cd506f3ab43dba8fc7deb85
OmarMesqq <omarmsqt@gmail.com>
2026-02-19 20:06:51 -0300

    build: :sparkles: add `customize.sh` to setup target apps dir

--- build_flashable_module.sh
+++ build_flashable_module.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Build the .so
+# Build the module's .so files
 cd module
 ndk-build
 cd ..
 
-# Copy built .so into Zygisk module project folder structure
+# Copy the freshly built shared libraries into Zygisk's expected module structure
 cd flashable_module
-rm -f bipan.zip
 mkdir -p zygisk
 cp ../module/libs/arm64-v8a/libbipan.so zygisk/arm64-v8a.so
 cp ../module/libs/armeabi-v7a/libbipan.so zygisk/armeabi-v7a.so
 
-# Create the zip with no compression
-zip -0 -r bipan.zip module.prop service.sh zygisk/ targets/
+# Create the final flashable zip with no compression
+rm -f ../bipan.zip
+zip -0 -r ../bipan.zip module.prop service.sh customize.sh zygisk/
---
+++ flashable_module/customize.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+
+ui_print "- Preparing target processes directory..."
+mkdir -p "$MODPATH/targets"
+
+ui_print "- Set at $MODPATH/targets"
+
+ui_print "- [!] You should now add files in this folder whose names are the packages to spoof"
+
+ui_print "- Bipan customization finished with no errors."
