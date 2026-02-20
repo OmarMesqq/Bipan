a0dd3a6b562eb348c8740902a8734c3abe28c7bb
OmarMesqq <omarmsqt@gmail.com>
2026-02-20 13:36:56 -0300

    docs: :memo: improve my understanding of `bipan_filters`

--- src/jni/bipan_filters.cpp
+++ src/jni/bipan_filters.cpp
@@ -9,90 +9,68 @@
 #include "bipan_shared.hpp"
 #include "bipan_filters.hpp"
 
-static void sigsys_trap_handler(int sig, siginfo_t *info, void *void_context) {
-    // Syscall the app called that triggered the trap
-    int nr = info->si_syscall;
+static void sigsys_trap_handler(int sig, siginfo_t *info, void *void_context);
+static void sigsys_log_handler(int sig, siginfo_t *info, void *void_context);
 
-    LOGE("--- BIPAN SANDBOX TRAP ---");
-    if (nr == __NR_execve || nr == __NR_execveat) {
-        LOGE("Violation: Attempted to EXECUTE a binary (Syscall %d)", nr);
-    } else if (nr == __NR_uname) {
-        LOGE("Violation: Attempted to call uname (Syscall %d)", nr);
-    } else {
-        LOGE("Violation: Blocked syscall %d", nr);
-    }
-    
-    _exit(1);
-}
-
-static void sigsys_log_handler(int sig, siginfo_t *info, void *void_context) {
-    ucontext_t *ctx = (ucontext_t *)void_context;
-    int nr = info->si_syscall;
-
-    LOGE("--- BIPAN SANDBOX LOG (PASSIVE) ---");
-    if (nr == __NR_execve) {
-        // X0 contains the pointer to the filename string
-        const char* path = (const char*)ctx->uc_mcontext.regs[0];
-        LOGE("Violation: execve(\"%s\")", path ? path : "NULL");
-    } 
-    else if (nr == __NR_execveat) {
-        // X1 contains the pointer to the filename string in execveat
-        const char* path = (const char*)ctx->uc_mcontext.regs[1];
-        LOGE("Violation: execveat(dfd, \"%s\")", path ? path : "NULL");
-    }
-    else if (nr == __NR_uname) {
-        // X0 contains the pointer to the utsname struct
-        LOGE("Violation: uname(%p)", (void*)ctx->uc_mcontext.regs[0]);
-    }
-    else {
-        LOGE("Violation: Blocked syscall %d", nr);
-    }
-
-    // 1. Advance the Program Counter (PC) by 4 bytes.
-    // On aarch64, 'svc #0' is exactly 4 bytes. Skipping it prevents the infinite loop.
-    ctx->uc_mcontext.pc += 4;
-
-    // 2. Set the return value in X0 to -EPERM (-1).
-    // The app receives this as the result of the "failed" syscall.
-    ctx->uc_mcontext.regs[0] = static_cast<unsigned long long>(-EPERM);
-}
-
+/**
+ * Berkeley Packet Filter program to
+ * block the following syscalls:
+ * - `execve`
+ * - `execveat`
+ * - `uname`
+ * 
+ * The kernel shall return `EPERM` to the program, whilst
+ * allowing other syscalls. Bipan can be ejected
+ * from memory in this case.
+ */
 static struct sock_filter blockFilter[] = {
-        // Load the syscall number into the accumulator
-        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, nr)),
-        
-        // Check for execve
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execve, 0, 1),
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)),
-
-        // Check for execveat
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execveat, 0, 1),
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)),
-
-        // Check for uname
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_uname, 0, 1),
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)),
-        
-        // If it didn't match those, allow it
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+    // Load the syscall number into the accumulator
+    BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, nr)),
+    
+    // If it's `execve`, block it
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execve, 0, 1),
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)),
+    // If it's `execveat`, block it
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execveat, 0, 1),
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)),
+    // If it's `uname`, block it
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_uname, 0, 1),
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ERRNO | (EPERM & SECCOMP_RET_DATA)),
+    
+    // Otherwise, allow it
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
 };
 
+/**
+ * Berkeley Packet Filter program to
+ * trap the following syscalls:
+ * - `execve`
+ * - `execveat`
+ * - `uname`
+ * 
+ * The kernel shall return `SIGSYS` to the program.
+ * For this to properly work, Bipan must stay in memory
+ * to install and maintain its signal handler during the app's
+ * lifetime.
+ */
 static struct sock_filter trapFilter[] = {
-        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, nr)),
-        
-        // Trap execve
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execve, 0, 1),
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
+    // Get the syscall's number
+    BPF_STMT(BPF_LD | BPF_W | BPF_ABS, offsetof(struct seccomp_data, nr)),
+    
+    // If it's `execve`, trap it
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execve, 0, 1),
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
 
-        // Trap execveat
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execveat, 0, 1),
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
+    // If it's `execveat`, trap it
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execveat, 0, 1),
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
 
-        // Trap uname
-        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_uname, 0, 1),
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
-        
-        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+    // If it's `uname`, trap it
+    BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_uname, 0, 1),
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
+    
+    // Otherwise, allow it
+    BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
 };
 
 void applySeccompFilter(BIPAN_FILTER opt) {
@@ -162,4 +140,60 @@ void applySeccompFilter(BIPAN_FILTER opt) {
     if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog) == -1) {
         LOGE("prctl(PR_SET_SECCOMP) failed: %d", errno);
     }
+}
+
+static void sigsys_trap_handler(int sig, siginfo_t *info, void *void_context) {
+    int nr = info->si_syscall;
+
+    LOGE("--- BIPAN SANDBOX TRAP ---");
+    if (nr == __NR_execve || nr == __NR_execveat) {
+        LOGE("Violation: Attempted to EXECUTE a binary (Syscall %d)", nr);
+    } else if (nr == __NR_uname) {
+        LOGE("Violation: Attempted to call uname (Syscall %d)", nr);
+    } else {
+        LOGE("Violation: Blocked syscall %d", nr);
+    }
+    
+    // Aggressively exit the program upon violation
+    _exit(1);
+}
+
+static void sigsys_log_handler(int sig, siginfo_t *info, void *void_context) {
+    ucontext_t *ctx = (ucontext_t *)void_context;
+    int nr = info->si_syscall;
+
+    LOGE("--- BIPAN SANDBOX LOG ---");
+    if (nr == __NR_execve) {
+        // X0 contains the pointer to the filename string
+        const char* path = (const char*)ctx->uc_mcontext.regs[0];
+        LOGE("Violation: execve(\"%s\")", path ? path : "NULL");
+    } 
+    else if (nr == __NR_execveat) {
+        // X1 contains the pointer to the filename string in execveat
+        const char* path = (const char*)ctx->uc_mcontext.regs[1];
+        LOGE("Violation: execveat(dfd, \"%s\")", path ? path : "NULL");
+    }
+    else if (nr == __NR_uname) {
+        // X0 contains the pointer to the utsname struct
+        LOGE("Violation: uname to the utsname struct at %p", (void*)ctx->uc_mcontext.regs[0]);
+    }
+    else {
+        LOGE("Violation: Blocked syscall %d", nr);
+    }
+
+    /**
+     * Increment the `pc` (Program Counter) by
+     * 4 bytes as to skip the Supervisor Call
+     * (`svc #0`) of aarch64.
+     * This "pretends" the syscall happened to the target
+     * program.
+     */
+    ctx->uc_mcontext.pc += 4;
+
+    /**
+     * Then, we mock the syscall's result.
+     * Here I'm denying it by
+     * placing `EPERM` on x0 (return register in aarch64)
+     */
+    ctx->uc_mcontext.regs[0] = static_cast<unsigned long long>(-EPERM);
 }
