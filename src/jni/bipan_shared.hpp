654b6a0cbf60003f4b025f34174ec0e9e5a0764f
OmarMesqq <omarmsqt@gmail.com>
2026-02-25 12:24:55 -0300

    refactor(bipan): :loud_sound: create `LOGW` and add newline to some files bc I'm Unixy

--- src/jni/bipan_shared.hpp
+++ src/jni/bipan_shared.hpp
@@ -5,6 +5,7 @@
 
 #define TAG "Bipan"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
 #endif
