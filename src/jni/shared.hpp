37988998169539b88e89fb7e46065f8d3de110a5
OmarMesqq <omarmsqt@gmail.com>
2026-02-26 09:36:48 -0300

    refactor(bipan): :sparkles: define IPC memory types

--- src/jni/shared.hpp
+++ src/jni/shared.hpp
@@ -8,4 +8,26 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 
+enum BROKER_STATUS {
+  IDLE = 0,
+  REQUEST_SYSCALL = 1,
+  BROKER_ANSWERED = 2
+};
+
+typedef struct {
+  volatile int status;
+
+  int nr; // syscall number
+
+  // arguments
+  long arg0;
+  char path[256];  // arg1 is a string pointer, so this takes its contents
+  long arg2, arg3, arg4, arg5;
+  
+  long ret;     // return value provided by kernel
+} SharedIPC;
+
+extern SharedIPC* ipc_mem;
+
+
 #endif
