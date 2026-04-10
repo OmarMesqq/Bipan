0e0756359fe2a7532a11f96e32f1910a2df5bad1
OmarMesqq <omarmsqt@gmail.com>
2026-04-09 22:15:20 -0300

    build: :boom: statically link to Dobby, drop `libcxx` in favour of `c++_static`

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -1,22 +1,33 @@
 LOCAL_PATH := $(call my-dir)
 
+# Include Dobby's compiled static lib in the project
+include $(CLEAR_VARS)
+LOCAL_MODULE := dobby_static
+LOCAL_SRC_FILES := libdobby.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+# Build Bipan
 include $(CLEAR_VARS)
 LOCAL_MODULE := bipan
-LOCAL_SRC_FILES := bipan.cpp blocker.cpp broker.cpp filter.cpp root_companion.cpp sigsys_handler.cpp spoofer.cpp
-LOCAL_STATIC_LIBRARIES := libcxx
-LOCAL_CPPFLAGS := -Wall -Wextra -Wconversion -Wsign-conversion \
-                  -Wdouble-promotion -Winline -fno-exceptions -fno-rtti \
+LOCAL_SRC_FILES := bipan.cpp \
+	 									blocker.cpp \
+										broker.cpp \
+										filter.cpp \
+										root_companion.cpp \
+										sigsys_handler.cpp \
+										spoofer.cpp
+
+
+# Statically link Bipan to Dobby
+LOCAL_STATIC_LIBRARIES := dobby_static
+
+LOCAL_CPPFLAGS := -Wall -Wextra \
+									-Wconversion -Wsign-conversion \
+                  -Wdouble-promotion -Winline \
+									-fno-exceptions -fno-rtti \
                   -fvisibility=hidden -fvisibility-inlines-hidden
-LOCAL_LDLIBS := -llog
+
+# "Local Linker Libraries": dynamically link to liblog.so (for use of logcat)
+LOCAL_LDLIBS := -llog 
+
 include $(BUILD_SHARED_LIBRARY)
-
-include jni/libcxx/Android.mk
-
-# If you do not want to use libc++, link to system stdc++
-# so that you can at least call the new operator in your code
-
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := example
-# LOCAL_SRC_FILES := example.cpp
-# LOCAL_LDLIBS := -llog -lstdc++
-# include $(BUILD_SHARED_LIBRARY)
