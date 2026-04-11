5c0ec996761818bf1d441fe895dcbb2d62ec88bc
OmarMesqq <omarmsqt@gmail.com>
2026-04-11 11:31:41 -0300

    simplify target code

--- Grunfeld/app/src/main/cpp/main.c
+++ Grunfeld/app/src/main/cpp/main.c
@@ -9,6 +9,7 @@
 #include <sys/utsname.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/syscall.h>
 
 #define TAG "Grunfeld"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
@@ -17,25 +18,18 @@
 static void get_uname();
 static void sigsys_log_handler(int sig, siginfo_t* info, void* void_context);
 static void install_sigsys_handler();
-static void scan_memory_maps();
-static void demo_fork_execve();
+
 
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   LOGD("Native C bridge initialized. Dumping system info...");
 
-  // uname + signal handler testing
   LOGD("Getting uname BEFORE sigsys handler installation");
   get_uname();
+
   LOGD("Getting uname AFTER sigsys handler installation");
   install_sigsys_handler();
   get_uname();
-
-  // execve/execveat testing
-  demo_fork_execve();
-
-  // /proc/self/maps hiding
-  scan_memory_maps();
-
+  
   return JNI_VERSION_1_6;
 }
 
@@ -74,130 +68,20 @@ static void get_uname() {
   }
 }
 
-static void demo_fork_execve() {
-  pid_t pid = fork();
-  if (pid == 0) {
-    sleep(1);
-    LOGD("Child: becoming '/sytem/bin/echo'...\n");
-    sleep(1);
-
-    char* args[] = {"/system/bin/echo", "Hello", NULL};
-    char* env[] = {NULL};
-
-    if (execve("/system/bin/echo", args, env) == -1) {
-      if (errno == EPERM) {  // Sandbox worked?
-        LOGE("Child: received permission denied (EPERM)");
-        _exit(13);
-      }
-    }
-    LOGE("Child: 'execve' failed due to UNKNOWN reason");
-    _exit(1);
-  } else {
-    int status;
-    LOGD("Parent: Waiting for child (PID %d) to finish...", pid);
-
-    if (waitpid(pid, &status, 0) == -1) {
-      LOGE("Parent: waitpid failed: %s", strerror(errno));
-    } else {
-      if (WIFSIGNALED(status)) {  // The process was terminated by a signal
-        int sig = WTERMSIG(status);
-        LOGE("Parent: Child terminated by SIGNAL %d (%s)", sig, strsignal(sig));
-      } else if (WIFEXITED(status)) {  // The process exited normally by itself
-        int exit_code = WEXITSTATUS(status);
-        if (exit_code == 0) {
-          LOGD("Parent: Child exited normally (status code %d)", exit_code);
-        } else {
-          LOGE("Parent: Child exited with FAILURE (status code %d)", exit_code);
-        }
-      } else {
-        LOGE("Parent: Child failed by UNKNOWN cause (status code: %d)", status);
-      }
-    }
-  }
-}
-
-static void scan_memory_maps() {
-  FILE* fp;
-  char line[1024];
-
-  fp = fopen("/proc/self/maps", "r");
-  if (fp == NULL) {
-    LOGE("Error opening /proc/self/maps");
-    return;
-  }
-
-  printf("Memory map BELOW:\n");
-  printf("--------------------------------------------------\n");
-
-  while (fgets(line, sizeof(line), fp)) {
-    // Here you can use sscanf to extract specific fields
-    // For now, we'll just print the raw line
-    LOGD("%s", line);
-  }
-
-  long long start, end;
-  char perms[5];
-  long long offset;
-  int dev_major, dev_minor;
-  long inode;
-  char path[1024];
-
-  if (sscanf(line, "%llx-%llx %4s %llx %x:%x %ld %s",
-             &start, &end, perms, &offset, &dev_major, &dev_minor, &inode, path) >= 7) {
-    LOGD("Range: %llx to %llx | Perms: %s | Path: %s\n", start, end, perms, path);
-  } else {
-    LOGE("sscanf failed to read all fields of /proc/self/maps");
-    fclose(fp);
-    return;
-  }
-
-  fclose(fp);
-  return;
-}
-
 static void install_sigsys_handler() {
   struct sigaction sa = {0};
   sa.sa_sigaction = sigsys_log_handler;
   sa.sa_flags = SA_SIGINFO;
-  int sigactionRet = sigaction(SIGSYS, &sa, NULL);
+
+  long sigactionRet = syscall(__NR_rt_sigaction, SIGSYS, (long)&sa, 0, 8);
   if (sigactionRet != 0) {
-    LOGE("failed to set SIGSYS handler (errno: %d)", errno);
+    LOGE("Failed to set SIGSYS handler (errno: %d)", errno);
     return;
   }
-  LOGD("Installed SIGSYS handler successfuly. Return value: %d", sigactionRet);
+  LOGD("Installed SIGSYS handler successfully. Return value: %ld", sigactionRet);
 }
 
 static void sigsys_log_handler(int sig, siginfo_t* info, void* void_context) {
-  ucontext_t* ctx = (ucontext_t*)void_context;
-  uintptr_t pc = ctx->uc_mcontext.pc;
-  uintptr_t lr = ctx->uc_mcontext.regs[30];
-  int nr = info->si_syscall;  // syscalls go in x8 in aarch64
-
-  long arg0 = ctx->uc_mcontext.regs[0];
-  long arg1 = ctx->uc_mcontext.regs[1];
-  long arg2 = ctx->uc_mcontext.regs[2];
-  long arg3 = ctx->uc_mcontext.regs[3];
-  long arg4 = ctx->uc_mcontext.regs[4];
-  long arg5 = ctx->uc_mcontext.regs[5];
-  long result = 0;
-
-  switch (nr) {
-    case 160: {
-      LOGD("Running legit uname");
-      break;
-    }
-    default: {
-      LOGE("Got another syscall: %d", nr);
-      break;
-    }
-  }
-  result = syscall(nr,
-                   ctx->uc_mcontext.regs[0],
-                   ctx->uc_mcontext.regs[1],
-                   ctx->uc_mcontext.regs[2],
-                   ctx->uc_mcontext.regs[3],
-                   ctx->uc_mcontext.regs[4],
-                   ctx->uc_mcontext.regs[5]);
-
-  ctx->uc_mcontext.regs[0] = result;
+  LOGE("Should never reach here...");
+  _exit(-1);
 }
