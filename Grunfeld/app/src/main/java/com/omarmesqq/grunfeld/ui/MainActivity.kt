74c0972098bb9e80f3c235b04e82d6de6541555c
OmarMesqq <omarmsqt@gmail.com>
2026-04-11 11:04:10 -0300

    dont overlap main screen with status bar

--- Grunfeld/app/src/main/java/com/omarmesqq/grunfeld/ui/MainActivity.kt
+++ Grunfeld/app/src/main/java/com/omarmesqq/grunfeld/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
@@ -53,6 +54,7 @@ fun InfoScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .safeDrawingPadding()
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
