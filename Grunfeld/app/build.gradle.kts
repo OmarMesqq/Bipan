4ca8acb6fa3ea7a37f8efa33f74b176b2fa131cb
OmarMesqq <omarmsqt@gmail.com>
2026-04-11 13:36:32 -0300

    add `leakCanary` and `lifecycleViewmodelCompose`

--- Grunfeld/app/build.gradle.kts
+++ Grunfeld/app/build.gradle.kts
@@ -65,4 +65,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    debugImplementation(libs.leakcanary.android)
 }
