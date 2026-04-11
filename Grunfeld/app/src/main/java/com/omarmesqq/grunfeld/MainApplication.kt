e2bd3569ce98334a67fdfc9637e3ebadc819e2af
OmarMesqq <omarmsqt@gmail.com>
2026-04-11 14:42:46 -0300

    `setDefaultUncaughtExceptionHandler`

--- Grunfeld/app/src/main/java/com/omarmesqq/grunfeld/MainApplication.kt
+++ Grunfeld/app/src/main/java/com/omarmesqq/grunfeld/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.omarmesqq.grunfeld
 
 import android.app.Application
+import android.util.Log
 
 class MainApplication: Application() {
     /**
@@ -10,6 +11,16 @@ class MainApplication: Application() {
     companion object {
         init {
             System.loadLibrary("grunfeld")
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e("CRITICAL_ERROR", "Uncaught exception in ${thread.name}", throwable)
+            // Forward the crash to the system (shows the "App has stopped" dialog)
+            defaultHandler?.uncaughtException(thread, throwable)
         }
     }
 }
