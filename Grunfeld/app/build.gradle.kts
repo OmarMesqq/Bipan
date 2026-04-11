1f86f144da7df2c9237294777639fbe5d0daaa3c
OmarMesqq <omarmsqt@gmail.com>
2026-04-11 10:36:32 -0300

    rename project

--- Grunfeld/app/build.gradle.kts
+++ Grunfeld/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.omarmesqq.bipantest"
+    namespace = "com.omarmesqq.grunfeld"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.omarmesqq.bipantest"
+        applicationId = "com.omarmesqq.grunfeld"
         minSdk = 33
         targetSdk = 36
         versionCode = 1
--- Grunfeld/settings.gradle.kts
+++ Grunfeld/settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "BipanTest"
+rootProject.name = "Grunfeld"
 include(":app")
