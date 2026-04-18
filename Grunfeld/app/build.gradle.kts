634b39af9180cd180ffe1d98c425e72aafb7afca
OmarMesqq <omarmsqt@gmail.com>
2026-04-17 23:34:13 -0300

    build(Grunfeld): :heavy_plus_sign: add okhttp

--- Grunfeld/app/build.gradle.kts
+++ Grunfeld/app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.webkit)
+    implementation(libs.okhttp)
 
     debugImplementation(libs.leakcanary.android)
 }
