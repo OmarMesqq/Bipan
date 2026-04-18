1022b23c75563495c3e50276fae1b676e2c7bea1
OmarMesqq <omarmsqt@gmail.com>
2026-04-18 10:49:07 -0300

    create Avocado logger

--- Grunfeld/app/src/main/java/com/omarmesqq/grunfeld/MainApplication.kt
+++ Grunfeld/app/src/main/java/com/omarmesqq/grunfeld/MainApplication.kt
@@ -5,18 +5,7 @@ import android.content.res.Configuration
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.os.StrictMode.VmPolicy
-import com.omarmesqq.grunfeld.utils.UIUtils.showToastAndLog
-import android.util.Log
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.net.InetAddress
-import androidx.webkit.WebViewCompat
-import androidx.webkit.WebViewOutcomeReceiver
-import androidx.webkit.WebViewStartUpConfig
-import androidx.webkit.WebViewStartUpResult
-import androidx.webkit.WebViewStartupException
-import java.util.concurrent.Executors
+import com.omarmesqq.grunfeld.utils.Avocado.avocadoLog
 
 
 private  const val TAG = "MainApplication"
@@ -31,7 +20,7 @@ class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
-            showToastAndLog(this, "App is debuggable...")
+            avocadoLog(this, "App is debuggable...")
             StrictMode.setThreadPolicy(
                 ThreadPolicy.Builder()
                     .detectCustomSlowCalls()
@@ -84,28 +73,28 @@ class MainApplication: Application() {
 
         val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            showToastAndLog(this, "CRITICAL_ERROR: Uncaught exception in ${thread.name}: $throwable")
+            avocadoLog(this, "CRITICAL_ERROR: Uncaught exception in ${thread.name}: $throwable")
             defaultHandler?.uncaughtException(thread, throwable)
         }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        showToastAndLog(this, "onConfigurationChanged")
+        avocadoLog(this, "onConfigurationChanged")
     }
 
     override fun onTrimMemory(level: Int) {
         super.onTrimMemory(level)
         // Release any resources that can be rebuilt quickly when the app returns to the foreground
         if (level >= TRIM_MEMORY_BACKGROUND) {
-            showToastAndLog(this, "onTrimMemory above TRIM_MEMORY_BACKGROUND")
+            avocadoLog(this, "onTrimMemory above TRIM_MEMORY_BACKGROUND")
         }
         // Release UI elements
         else if (level >= TRIM_MEMORY_UI_HIDDEN) {
-            showToastAndLog(this, "onTrimMemory above TRIM_MEMORY_UI_HIDDEN")
+            avocadoLog(this, "onTrimMemory above TRIM_MEMORY_UI_HIDDEN")
         }
         else {
-            showToastAndLog(this, "onTrimMemory unknown level: $level")
+            avocadoLog(this, "onTrimMemory unknown level: $level")
         }
     }
 
@@ -114,6 +103,6 @@ class MainApplication: Application() {
      */
     override fun onLowMemory() {
         super.onLowMemory()
-        showToastAndLog(this, "onLowMemory")
+        avocadoLog(this, "onLowMemory")
     }
 }
