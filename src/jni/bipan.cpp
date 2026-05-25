ac043eed43fea19738dad234e4811747920e0d8e
OmarMesqq <omarmsqt@gmail.com>
2026-05-25 15:22:25 -0300

    revert(Bipan): :rewind: undo get prop hooks

--- src/jni/bipan.cpp
+++ src/jni/bipan.cpp
@@ -126,7 +126,6 @@ class Bipan : public zygisk::ModuleBase {
     if (isTargetApp) {
       // registerDobbyLinkerHooks();
       registerDobbySensorsHooks();
-      registerDobbyPropertyHooks();
 
       LibBounds my_lib;
       dl_iterate_phdr(find_lib_bounds, &my_lib);
