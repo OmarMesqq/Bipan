1a19b02e76c45ecdb93bafb4560fdfd32290d00d
OmarMesqq <omarmsqt@gmail.com>
2026-06-12 18:56:52 -0300

    feat(BipanJava): :sparkles: add `spoofOsVersion`

--- src/com/omarmesqq/bipan/BipanJava.java
+++ src/com/omarmesqq/bipan/BipanJava.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 import android.app.Instrumentation;
@@ -26,7 +27,6 @@ public class BipanJava {
   private static final AtomicBoolean instrumentationHooked = new AtomicBoolean(false);
 
   private static final long WAIT_UNTIL_MODULES_READY_TIMEOUT_MS = 15000;
-
 
   /**
    * Phase 2: called from C++ my_clampGrowthLimit / my_clearGrowthLimit.
@@ -104,7 +104,8 @@ public class BipanJava {
       mThreadField.set(hooked, mThreadField.get(realInstr));
 
       mInstrField.set(at, hooked);
-      // Log.i(TAG, "mInstrumentation hooked. This should block before ContentProviders and onCreate.");
+      // Log.i(TAG, "mInstrumentation hooked. This should block before
+      // ContentProviders and onCreate.");
     } catch (Exception e) {
       Log.e(TAG, "hookInstrumentationNow failed: ", e);
     }
@@ -156,6 +157,8 @@ public class BipanJava {
   }
 
   private static void loadModules(Context context) {
+    spoofOsVersion();
+
     List<BaseHook> modules = new ArrayList<>();
 
     modules.add(new AntiAppSweepingHook());
@@ -211,6 +214,26 @@ public class BipanJava {
       } catch (Throwable e2) {
         Log.e(TAG, "Fatal: Could not unseal VM", e2);
       }
+    }
+  }
+
+  /**
+   * https://cs.android.com/android/platform/superproject/+/android-latest-release:libcore/ojluni/src/main/java/java/lang/System.java;l=1799?q=java.lang.System&ss=android%2Fplatform%2Fsuperproject
+   */
+  private static void spoofOsVersion() {
+    try {
+      // System.java overrides setProperty to ignore "protected" props
+      Class<?> systemClass = Class.forName("java.lang.System");
+      Field unchangeablePropsField = systemClass.getDeclaredField("unchangeableProps");
+
+      // `unchangeableProps` is a static field in current AOSP, so we can just put()
+      unchangeablePropsField.setAccessible(true);
+      Properties unchangeableProps = (Properties) unchangeablePropsField.get(null);
+
+      // Bypasses PropertiesWithNonOverrideableDefaults "protections"
+      unchangeableProps.put("os.version", "6.6.56-android16-11-g8a3e2b1c4d5f");
+    } catch (Exception e) {
+      Log.wtf(TAG, "[!] Failed to spoof os.version: " + e);
     }
   }
 }
