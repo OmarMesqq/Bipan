b09a70ff154664cb0d85606c504d15a1e674b574
OmarMesqq <omarmsqt@gmail.com>
2026-07-19 18:12:13 -0300

    build: differentiate linux and mac + skip symbol stripping in debug

--- build_module.sh
+++ build_module.sh
@@ -40,14 +40,30 @@ cd ..
 cd module
 mkdir -p zygisk
 cp ../src/libs/arm64-v8a/libbipan.so zygisk/arm64-v8a.so
-$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-objcopy \
-  --strip-all \
-  -R .eh_frame \
-  -R .eh_frame_hdr \
-  -R .gcc_except_table \
-  -R .note.gnu.build-id \
-  -R .note.android.ident \
-  ./zygisk/arm64-v8a.so
+
+if [[ "${1:-}" == "debug" ]]; then
+  echo "Skipping symbol stripping"
+else
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    $NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-objcopy \
+      --strip-all \
+      -R .eh_frame \
+      -R .eh_frame_hdr \
+      -R .gcc_except_table \
+      -R .note.gnu.build-id \
+      -R .note.android.ident \
+      ./zygisk/arm64-v8a.so
+  else
+    $NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-objcopy \
+      --strip-all \
+      -R .eh_frame \
+      -R .eh_frame_hdr \
+      -R .gcc_except_table \
+      -R .note.gnu.build-id \
+      -R .note.android.ident \
+      ./zygisk/arm64-v8a.so
+  fi
+fi
 
 # Create the final flashable zip with no compression
 rm -f ../bipan.zip
