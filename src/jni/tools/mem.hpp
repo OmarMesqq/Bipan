e8bfe55c9b678ea5bd46e274de1df2fa812596f7
OmarMesqq <omarmsqt@gmail.com>
2026-07-20 12:10:16 -0300

    refactor(Bipan): :truck: move `findBipansBounds` and `scrubBipansElfHeader` into `bipan.cpp`

--- src/jni/tools/mem.hpp
+++ src/jni/tools/mem.hpp
@@ -3,15 +3,8 @@
 
 #include <stdint.h>
 
-struct LibBounds {
-  uintptr_t start = 0;
-  uintptr_t end = 0;
-};
-
-int findBipansBounds(struct dl_phdr_info* info, size_t size, void* data);
 void dumpBytes(void* addr, int bytes);
 int dumpBipanLinkerInfo(struct dl_phdr_info* info, size_t size, void* data);
 void readAuxVector();
-bool scrubBipansElfHeader();
 
 #endif
