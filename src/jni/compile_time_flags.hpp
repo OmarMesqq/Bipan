32f4dce4a6918cb83876ea3342a60c844d7aa69a
OmarMesqq <omarmsqt@gmail.com>
2026-07-21 19:32:34 -0300

    feat(Broker): :sparkles: add a new `BROKER_EXPERIMENTS` flag + log and nullify SIGSEGV and SIGQUIT handlers (TODO)

--- src/jni/compile_time_flags.hpp
+++ src/jni/compile_time_flags.hpp
@@ -1,16 +1,16 @@
 #ifndef COMPILE_TIME_FLAGS_HPP
 #define COMPILE_TIME_FLAGS_HPP
 
-// Enables debug logging for injected code
 // #define IN_APP_DEBUG_LOGGING
 
-// Enables debug logging for Broker
+// For now enables Dobby hooking of linker lib-loading functions
+#define IN_APP_EXPERIMENTS
+
+
 #define BROKER_DEBUG_LOGGING
+#define BROKER_EXPERIMENTS
 
 // Syscalls I am still trying to investigate severity and how "hot" they are
-// #define TRAP_EXPERIMENTAL_SYSCALLS
-
-// For now enables Dobby hooking of linker lib-loading functions
-// #define IN_APP_EXPERIMENTS
+#define TRAP_EXPERIMENTAL_SYSCALLS
 
 #endif
