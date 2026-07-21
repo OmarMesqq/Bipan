c672f10d6e4a67dc78a2f4b238f78b6fd0a91650
OmarMesqq <omarmsqt@gmail.com>
2026-07-21 19:34:01 -0300

    build: :building_construction: improve `Application.mk` for debug/release variants

--- src/jni/Application.mk
+++ src/jni/Application.mk
@@ -1,4 +1,20 @@
 APP_ABI      := arm64-v8a
-APP_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden
 APP_STL      := c++_static
 APP_PLATFORM := android-28
+
+# TODO: add the following to debug builds (requires battling with APP_STL)
+# APP_CFLAGS 	 := -fsanitize=hwaddress
+# APP_LDFLAGS  := -fsanitize=hwaddress
+ 
+ifeq ($(BIPAN_DEBUG), 1)
+  APP_OPTIM    := debug
+  APP_CPPFLAGS := -std=c++17 \
+    -fno-exceptions -fno-rtti \
+    -fno-omit-frame-pointer
+else
+  APP_OPTIM    := release
+  APP_CPPFLAGS := -std=c++17 \
+    -fno-exceptions -fno-rtti \
+    -fvisibility=hidden -fvisibility-inlines-hidden \
+    -fomit-frame-pointer
+endif
