26a82cb718d1b7536260e60c4712839d34214c09
OmarMesqq <omarmsqt@gmail.com>
2026-07-24 15:56:34 -0300

    refactor(Bipan): :recycle: add `IN_APP_PERF_ANALYSIS` flag

--- src/jni/compile_time_flags.hpp
+++ src/jni/compile_time_flags.hpp
@@ -1,23 +1,39 @@
 #ifndef COMPILE_TIME_FLAGS_HPP
 #define COMPILE_TIME_FLAGS_HPP
 
-// #define IN_APP_DEBUG_LOGGING
+#define IN_APP_DEBUG_LOGGING
 
-// For now enables Dobby hooking of linker lib-loading functions
-// #define IN_APP_EXPERIMENTS
+/**
+ * Enables:
+ * - Hooking of the linker's `dlopen` and `android_dlopen_ext` at `preAppSpecialize`
+ * - Zygisk's `FORCE_DENYLIST_UNMOUNT` at `preAppSpecialize`
+ * - Hooking of the linker's `dlsym` at `postAppSpecialize`
+ */
+#define IN_APP_EXPERIMENTS
+
+
+// #define IN_APP_SPOOF_GETIFADDRS
+
+
+// #define IN_APP_PERF_ANALYSIS
 
 /**
  * If enabled, the SIGSYS handler registration is done in assembly,
- * bypassing `bionic`'s wrappers.
- * 
- * Otherwise, it's done through the library provided `sigaction()`
+ * bypassing `bionic`'s wrappers and `libsigchain.so`.
+ *
+ * Otherwise, it's done through the library provided `sigaction()`.
+ *
+ * Still studying whether this is a good idea. Turning it off is
+ * definitely useful for debugging the lib as we don't overwrite
+ * `tombstoned`/`debuggerd` built-in handlers that generate dumps.
  */
 // #define IN_APP_RAW_SIGNAL_REGISTRATION
 
 // #define BROKER_DEBUG_LOGGING
-// #define BROKER_EXPERIMENTS
+
+// #define BROKER_DEBUG_BUILD
 
 // Syscalls I am still trying to investigate severity and how "hot" they are
-// #define TRAP_EXPERIMENTAL_SYSCALLS
+#define TRAP_EXPERIMENTAL_SYSCALLS
 
 #endif
