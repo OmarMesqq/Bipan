4e5d5a9fa76ec2dca8a5b377b53a547d8481173b
OmarMesqq <omarmsqt@gmail.com>
2026-07-28 00:18:21 -0300

    refactor(Bipan): :lock: allow loopback and unspecified addresses

--- src/jni/broker/policies.cpp
+++ src/jni/broker/policies.cpp
@@ -326,14 +326,14 @@ struct stat* fixHostsFileStat(const char* pathname, int flags) {
  */
 static inline bool is_ipv4_lan_addr(uint32_t ip4) {
   // Unspecified address (0.0.0.0)
-  if (ip4 == 0x00000000) {
-    return true;
-  }
+  // if (ip4 == 0x00000000) {
+  //   return true;
+  // }
 
   // Loopback (127.0.0.0/8)
-  if ((ip4 & 0xFF000000) == 0x7F000000) {
-    return true;
-  }
+  // if ((ip4 & 0xFF000000) == 0x7F000000) {
+  //   return true;
+  // }
 
   if ((ip4 & 0xFF000000) == 0x0A000000) {
     // 10.0.0.0/8 (Class A Private)
@@ -390,22 +390,22 @@ static inline bool is_ipv6_lan_addr(uint8_t* ip6) {
   }
 
   // Unspecified (::)
-  bool is_unspecified = true;
-  for (int i = 0; i < 16; i++) {
-    if (ip6[i] != 0) is_unspecified = false;
-  }
-  if (is_unspecified) {
-    return true;
-  }
+  // bool is_unspecified = true;
+  // for (int i = 0; i < 16; i++) {
+  //   if (ip6[i] != 0) is_unspecified = false;
+  // }
+  // if (is_unspecified) {
+  //   return true;
+  // }
 
   // Loopback (::1)
-  bool is_loopback = (ip6[15] == 1);
-  for (int i = 0; i < 15; i++) {
-    if (ip6[i] != 0) is_loopback = false;
-  }
-  if (is_loopback) {
-    return true;
-  }
+  // bool is_loopback = (ip6[15] == 1);
+  // for (int i = 0; i < 15; i++) {
+  //   if (ip6[i] != 0) is_loopback = false;
+  // }
+  // if (is_loopback) {
+  //   return true;
+  // }
 
   if (ip6[0] == 0xFE && (ip6[1] & 0xC0) == 0x80) {
     // fe80::/10 (Link-Local)
