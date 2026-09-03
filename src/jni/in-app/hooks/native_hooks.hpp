2c77f3b17230f46285f3a4c014fea1b3ae70e3dd
OmarMesqq <omarmsqt@gmail.com>
2026-09-03 18:20:27 -0300

    refactor(Bipan): :boom: introduce macros in the sig handler so its abi aware

--- src/jni/in-app/hooks/native_hooks.hpp
+++ src/jni/in-app/hooks/native_hooks.hpp
@@ -11,11 +11,11 @@
 
 #include <unordered_map>
 
+#include "../filter.hpp"
 #include "common_utils.hpp"
 #include "deps/dobby.h"
 #include "deps/zygisk.hpp"
 #include "drm_hook.hpp"
-#include "../filter.hpp"
 #include "in-app/globals.hpp"
 #include "logger/logger.hpp"
 
@@ -792,6 +792,48 @@ void registerDobbyDrmHook(void) {
   if (rc != 0) {
     write_to_logcat_async(ANDROID_LOG_FATAL, TAG, "[!] Failed to hook AMediaDrm_getPropertyByteArray!");
     return;
+  }
+}
+
+void registerDobbyBionicSignalHooks(void) {
+  const int bionicSignalMethodsCount = 7;
+  const char* symbols[] = {
+      "sigaction",
+      "signal",
+      "bsd_signal",
+      "sigprocmask",
+      "pthread_sigmask",
+      "sigsuspend",
+      "signalfd",
+  };
+
+  void* hooks[] = {
+      (void*)hook_ASensorManager_getInstance,
+      (void*)hook_ASensorManager_getInstanceForPackage,
+      (void*)hook_ASensorManager_getSensorList,
+      (void*)hook_ASensorManager_getDefaultSensor,
+      (void*)hook_ASensorManager_createEventQueue};
+
+  void** originals[] = {
+      (void**)&orig_ASensorManager_getInstance,
+      (void**)&orig_ASensorManager_getInstanceForPackage,
+      (void**)&orig_ASensorManager_getSensorList,
+      (void**)&orig_ASensorManager_getDefaultSensor,
+      (void**)&orig_ASensorManager_createEventQueue};
+
+  for (int i = 0; i < bionicSignalMethodsCount; i++) {
+    void* addr = dlsym(RTLD_DEFAULT, symbols[i]);
+    if (!addr) {
+      write_to_logcat_async(ANDROID_LOG_FATAL, TAG, "[!] Failed to resolve: %s", symbols[i]);
+      BIPAN_PANIC();
+    }
+
+    if (DobbyHook(addr, hooks[i], originals[i]) != 0) {
+      write_to_logcat_async(ANDROID_LOG_FATAL, TAG, "[!] Failed to hook: %s", symbols[i]);
+      BIPAN_PANIC();
+    }
+
+    __builtin___clear_cache((char*)addr, (char*)addr + 32);
   }
 }
 
