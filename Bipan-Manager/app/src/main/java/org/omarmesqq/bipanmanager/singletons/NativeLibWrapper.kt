05d217f1199e1a17365b3a65e8b2eef02572dc6d
OmarMesqq <omarmsqt@gmail.com>
2026-09-21 11:07:33 -0300

    refactor(Manager): :recycle: create `data` package

--- Bipan-Manager/app/src/main/java/org/omarmesqq/bipanmanager/singletons/NativeLibWrapper.kt
+++ Bipan-Manager/app/src/main/java/org/omarmesqq/bipanmanager/singletons/NativeLibWrapper.kt
@@ -1,10 +1,5 @@
 package org.omarmesqq.bipanmanager.singletons
 
-
-data class TemplateJniCrossingStruct(
-    val foo: String
-)
-
 object NativeLibWrapper {
     // external fun getFoo(s: String): String
 }
